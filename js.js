7a780b11b5d851b5a1b4fc15eeb2164bbb322c0b
winonomp <60493919+winonomp@users.noreply.github.com>
2020-04-08 00:21:39 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,134 @@
+
+
+//CONTENT 1
+$(document).ready(function(){
+    var boolDrop = true;
+    $("#drop1Link").click(function(){
+    if(boolDrop === true){
+    $("#drop1").css({ display : "none" });
+    $("#drop1Icon").css({ "-webkit-transform": "rotate(180deg)",
+                            "-moz-transform": "rotate(180deg)",
+                             "transform": "rotate(180deg)"});
+    $("#drop1Text").text("OPEN DETAILS");
+        boolDrop = false;
+    }
+    else if(boolDrop === false){
+        $("#drop1").css({ display : "block" });
+        $("#drop1Icon").css({ "-webkit-transform": "rotate(0deg)",
+                                "-moz-transform": "rotate(0deg)",
+                                "transform": "rotate(0deg)"});
+        $("#drop1Text").text("CLOSE DETAILS");
+    boolDrop = true;
+      }
+  });
+});
+
+$(document).ready(function(){
+    var boolDrop = true;
+    $("#drop2Link").click(function(){
+    if(boolDrop === true){
+    $("#drop2").css({ display : "none" });
+    $("#drop2Icon").css({ "-webkit-transform": "rotate(180deg)",
+                            "-moz-transform": "rotate(180deg)",
+                             "transform": "rotate(180deg)"});
+    $("#drop2Text").text("OPEN DETAILS");
+        boolDrop = false;
+    }
+    else if(boolDrop === false){
+        $("#drop2").css({ display : "block" });
+        $("#drop2Icon").css({ "-webkit-transform": "rotate(0deg)",
+                                "-moz-transform": "rotate(0deg)",
+                                "transform": "rotate(0deg)"});
+        $("#drop2Text").text("CLOSE DETAILS");
+    boolDrop = true;
+      }
+  });
+});
+
+$(document).ready(function(){
+    var boolDrop = true;
+    $("#drop3Link").click(function(){
+    if(boolDrop === true){
+    $("#drop3").css({ display : "none" });
+    $("#drop3Icon").css({ "-webkit-transform": "rotate(180deg)",
+                            "-moz-transform": "rotate(180deg)",
+                             "transform": "rotate(180deg)"});
+    $("#drop3Text").text("OPEN DETAILS");
+        boolDrop = false;
+    }
+    else if(boolDrop === false){
+        $("#drop3").css({ display : "block" });
+        $("#drop3Icon").css({ "-webkit-transform": "rotate(0deg)",
+                                "-moz-transform": "rotate(0deg)",
+                                "transform": "rotate(0deg)"});
+        $("#drop3Text").text("CLOSE DETAILS");
+    boolDrop = true;
+      }
+  });
+});
+
+$(document).ready(function(){
+    var boolDrop = true;
+    $("#drop4Link").click(function(){
+    if(boolDrop === true){
+    $("#drop4").css({ display : "none" });
+    $("#drop4Icon").css({ "-webkit-transform": "rotate(180deg)",
+                            "-moz-transform": "rotate(180deg)",
+                             "transform": "rotate(180deg)"});
+    $("#drop4Text").text("OPEN DETAILS");
+        boolDrop = false;
+    }
+    else if(boolDrop === false){
+        $("#drop4").css({ display : "block" });
+        $("#drop4Icon").css({ "-webkit-transform": "rotate(0deg)",
+                                "-moz-transform": "rotate(0deg)",
+                                "transform": "rotate(0deg)"});
+        $("#drop4Text").text("CLOSE DETAILS");
+    boolDrop = true;
+      }
+  });
+});
+
+$(document).ready(function(){
+    var boolDrop = true;
+    $("#drop5Link").click(function(){
+    if(boolDrop === true){
+    $("#drop5").css({ display : "none" });
+    $("#drop5Icon").css({ "-webkit-transform": "rotate(180deg)",
+                            "-moz-transform": "rotate(180deg)",
+                             "transform": "rotate(180deg)"});
+    $("#drop5Text").text("OPEN DETAILS");
+        boolDrop = false;
+    }
+    else if(boolDrop === false){
+        $("#drop5").css({ display : "block" });
+        $("#drop5Icon").css({ "-webkit-transform": "rotate(0deg)",
+                                "-moz-transform": "rotate(0deg)",
+                                "transform": "rotate(0deg)"});
+        $("#drop5Text").text("CLOSE DETAILS");
+    boolDrop = true;
+      }
+  });
+});
+
+$(document).ready(function(){
+    var boolDrop = true;
+    $("#drop6Link").click(function(){
+    if(boolDrop === true){
+    $("#drop6").css({ display : "none" });
+    $("#drop6Icon").css({ "-webkit-transform": "rotate(180deg)",
+                            "-moz-transform": "rotate(180deg)",
+                             "transform": "rotate(180deg)"});
+    $("#drop6Text").text("OPEN DETAILS");
+        boolDrop = false;
+    }
+    else if(boolDrop === false){
+        $("#drop6").css({ display : "block" });
+        $("#drop6Icon").css({ "-webkit-transform": "rotate(0deg)",
+                                "-moz-transform": "rotate(0deg)",
+                                "transform": "rotate(0deg)"});
+        $("#drop6Text").text("CLOSE DETAILS");
+    boolDrop = true;
+      }
+  });
+});
